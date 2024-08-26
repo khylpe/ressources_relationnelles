88b0bde962c47a19b8eaee9bc686393ea072aae6
CaprycOrne <nico.ch007@gmail.com>
2024-08-26 14:28:00 +0200

    update Cors

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -12,7 +12,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://cube.aio-tools.com','*'],
 
     'allowed_origins_patterns' => [],
 
